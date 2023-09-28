f39d467e548f797b8bc372cbb589eca1d8bdeec9
Masum Billah <office.hellomasum@gmail.com>
2023-09-28 04:52:30 -0700

    printing html using php buffering

--- admin/partials/maintenance-mode-by-monk-settings.php
+++ admin/partials/maintenance-mode-by-monk-settings.php
@@ -5,18 +5,25 @@ class Mmbm_Settings{
         add_action( 'admin_init', [$this, 'mmbm_loading_settings_markup'] );
     }
 
-    public static function mmbm_loading_settings_markup(){
-        $markup = '<div class="wrap">
-                    <h1 class="wp-heading-inline"> Maintenance Mode by MONK - Settings</h1>
-                    <hr>
-                    <div>
-                        <label class="switch">
-                            <input type="checkbox" name="checkbox" checked>
-                            <span class="slider round"></span>
-                        </label>
-                        <span class="on_off_text">ON</span>
-                    </div>
-                </div>';
+    public static function mmbm_loading_settings_markup(){ 
+
+        ob_start();
+        ?>
+         <div class="wrap">
+            <h1 class="wp-heading-inline"> Maintenance Mode by MONK - Settings</h1>
+            <hr>
+            <div>
+                <label class="switch">
+                    <input type="checkbox" name="checkbox" checked>
+                    <span class="slider round"></span>
+                </label>
+                <span class="on_off_text">ON</span>
+            </div>
+        </div>
+
+        <?php 
+
+        $markup = ob_get_clean();
 
         return $markup;
     }
